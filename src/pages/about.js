474da1f070fdab9c55cc24aa1005ad0de16f1b7c
Mel DeJesus <meldejesus22@gmail.com>
2019-06-17 12:44:38 -0400

    fix

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,19 +7,15 @@ function Info() {
     <ColContainer2 style={{ marginRight: "20vw", marginLeft: "20vw" }}>
       <Title> Background </Title>
 
-      <ParaText>
-      I tend to focus on javaScript with regular excursions into React, Node, Express, and the NoSQL and SQL query languages.
+      <ParaText>I tend to focus on javaScript with regular excursions into React, Node, Express, and the NoSQL and SQL query languages.
       </ParaText>
 
-      <ParaText>
-        In my previous life, I studied literature and philosophy and taught writing, research, and outdoor classes at various nearby
+      <ParaText>In my previous life, I studied literature and philosophy and taught writing, research, and outdoor classes at various nearby
         colleges and centers.
       </ParaText>
-      <ParaText>
-        Coding could be seen as a kind of writing, with its own syntax and rhetoric, its own recursive process, its own modes of self-expression, collaboration, and community.
+      <ParaText>Coding could be seen as a kind of writing, with its own syntax and rhetoric, its own recursive process, its own modes of self-expression, collaboration, and community.
       </ParaText>
-      <ParaText>
-        These days, I spend my time with my wife, daughter, and dog.
+      <ParaText>These days, I spend my time with my wife, daughter, and dog.
       </ParaText>
     </ColContainer2>
   );
